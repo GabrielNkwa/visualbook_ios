272e9533e7fb59306e16ddc7b52335552ec81c2e
Gabriel Nkwa <gnkwa@outlook.com>
2023-08-10 14:05:09 +0100

    Initial Commit

---
+++ visualbook/visualbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  visualbookApp.swift
+//  visualbook
+//
+//  Created by visual book  on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct visualbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
